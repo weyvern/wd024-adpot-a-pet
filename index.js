18cd72f9ccf66b28c192700b6b264a4834ab2528
Jorge Paul <jserranopaul@gmail.com>
2022-04-04 16:02:37 +0200

    pet shelter

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 import express from 'express';
+import pets from './pets.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/animals/:type', (req, res) => {
+  const {
+    params: { type }
+  } = req;
+  const species = ['dogs', 'cats', 'rabbits'];
+  const availablePets = pets[type];
+  species.includes(type.toLowerCase())
+    ? res.render('animalType', { type, availablePets })
+    : res.render('notFound', { msg: `Sorry, we don't have those animals here` });
+});
+
+app.get('/animals/:type/:id', (req, res) => {
+  const {
+    params: { type, id }
+  } = req;
+  const availablePets = pets[type] || [];
+  const pet = availablePets.find(pet => pet.id === parseInt(id));
+  pet
+    ? res.render('singlePet', { pet })
+    : res.render('notFound', { msg: `The pet you are trying to visit, is not here` });
+});
+
+app.get('*', (req, res) =>
+  res.render('notFound', { msg: `The link you are trying to access doesn't exist` })
+);
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
